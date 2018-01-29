f38e52e1bf80b6558b32d04329cce115e3ba2afa
Matt Juszczyk <juszczy2@msu.edu>
2018-01-29 18:53:21 -0500

    JS REFORMAT..

--- assets/dist/js/script.js
+++ assets/dist/js/script.js
@@ -1,120 +1,116 @@
 // Navbar indicator slider
-$( window ).load(function() {
-	// Set offsets of slides
-	var navHeight=$("nav.navbar").height();
-	var slideOne=$(".slide#slide1").offset().top - navHeight,
-		slideTwo=$(".slide#slide2").offset().top - navHeight,
-		slideThree=$(".slide#slide3").offset().top - navHeight,
-		slideFour=$(".slide#slide4").offset().top - navHeight,
-		slideFive=$(".slide#slide5").offset().top - navHeight,
-		totalHeight=$("#body").height();
-	var indicatorOne={
-			leftOffset: $("#nav1").offset().left,
-			width: $("#nav1").width(),
-			color: "#FFF"
-		},
-		indicatorTwo={
-			leftOffset: $("#nav2").offset().left,
-			width: $("#nav2").width(),
-			color: "#1c1a1a"
-		},
-		indicatorThree={
-			leftOffset: $("#nav3").offset().left,
-			width: $("#nav3").width(),
-			color: "#3f0e0e"
-		},
-		indicatorFour={
-			leftOffset: $("#nav4").offset().left,
-			width: $("#nav4").width(),
-			color: "#9d0000"
-		},
-		indicatorFive={
-			leftOffset: $("#nav5").offset().left,
-			width: $("#nav5").width(),
-			color: "#303030"
-		};
-	// Set the css of #indicator here, so it shows up correct on load
-	$("#indicator").css("left",indicatorOne.leftOffset).css("width",indicatorOne.width);
+$(window).load(function() {
+    // Set offsets of slides
+    var navHeight = $("nav.navbar").height();
+    var slideOne = $(".slide#slide1").offset().top - navHeight,
+        slideTwo = $(".slide#slide2").offset().top - navHeight,
+        slideThree = $(".slide#slide3").offset().top - navHeight,
+        slideFour = $(".slide#slide4").offset().top - navHeight,
+        slideFive = $(".slide#slide5").offset().top - navHeight,
+        totalHeight = $("#body").height();
+    var indicatorOne = {
+            leftOffset: $("#nav1").offset().left,
+            width: $("#nav1").width(),
+            color: "#FFF"
+        },
+        indicatorTwo = {
+            leftOffset: $("#nav2").offset().left,
+            width: $("#nav2").width(),
+            color: "#1c1a1a"
+        },
+        indicatorThree = {
+            leftOffset: $("#nav3").offset().left,
+            width: $("#nav3").width(),
+            color: "#3f0e0e"
+        },
+        indicatorFour = {
+            leftOffset: $("#nav4").offset().left,
+            width: $("#nav4").width(),
+            color: "#9d0000"
+        },
+        indicatorFive = {
+            leftOffset: $("#nav5").offset().left,
+            width: $("#nav5").width(),
+            color: "#303030"
+        };
+    // Set the css of #indicator here, so it shows up correct on load
+    $("#indicator").css("left", indicatorOne.leftOffset).css("width", indicatorOne.width);
 
-	var progressBars=false;
+    var progressBars = false;
 
     // scroll indicator
-	$(window).scroll(function(){
-		// Distance meseaured from top
-		var scrollPosition = window.scrollY;
-		// Distance measured from bottom
-		var fullScrollPosition = $(window).scrollTop() + $(window).height();
+    $(window).scroll(function() {
+        // Distance meseaured from top
+        var scrollPosition = window.scrollY;
+        // Distance measured from bottom
+        var fullScrollPosition = $(window).scrollTop() + $(window).height();
 
-		var percentScroll = ((fullScrollPosition/totalHeight)*100);
-		// mobileIndicator.css("width",(percentScroll.toString() + '%'));
-		// This just kinda seems weird on mobile
-		// since the load progress bar is in same spot
+        var percentScroll = ((fullScrollPosition / totalHeight) * 100);
+        // mobileIndicator.css("width",(percentScroll.toString() + '%'));
+        // This just kinda seems weird on mobile
+        // since the load progress bar is in same spot
 
-		// console.log("fullScrollPosition: " + fullScrollPosition + "; totalHeight: " + totalHeight + "; percentScroll: " + percentScroll);
+        // console.log("fullScrollPosition: " + fullScrollPosition + "; totalHeight: " + totalHeight + "; percentScroll: " + percentScroll);
 
-		if (percentScroll <= 96) {
-			if (scrollPosition < slideTwo && scrollPosition > slideOne){
-				$("#indicator").css("left",indicatorOne.leftOffset)
-					.css("width",indicatorOne.width)
-					.css("background-color",indicatorOne.color);
-			}
-			else if (scrollPosition < slideThree && scrollPosition > slideTwo){
-				$("#indicator").css("left",indicatorTwo.leftOffset)
-					.css("width",indicatorTwo.width)
-					.css("background-color",indicatorTwo.color);
-			}
-			else if (scrollPosition < slideFour && scrollPosition > slideThree){
-				$("#indicator").css("left",indicatorThree.leftOffset)
-					.css("width",indicatorThree.width)
-					.css("background-color",indicatorThree.color);
-				if (scrollPosition > (slideThree * 1.4)){
-					$("#html-sk").css("width", "100%");
-			    	$("#css-sk").css("width", "100%");
-			    	$("#js-sk").css("width", "70%");
-                    $("#php-sk").css("width","65%");
-                    $("#twig-sk").css("width","80%");
-			    	$("#grail-sk").css("width","50%");
-			    	$("#groov-sk").css("width","80%");
-			    	$("#c-sk").css("width","70%");
-					$("#pyth-sk").css("width","50%");
-					progressBars=true;
-				}
-			}
-			else if (scrollPosition < slideFive && scrollPosition > slideFour){
-				$("#indicator").css("left",indicatorFour.leftOffset)
-					.css("width",indicatorFour.width)
-					.css("background-color",indicatorFour.color);
-			}
-		}
-		else {
-			$("#indicator").css("left",indicatorFive.leftOffset)
-				.css("width",indicatorFive.width)
-				.css("background-color",indicatorFive.color);
-			if (!progressBars){
-				$("#html-sk").css("width", "100%");
-		    	$("#css-sk").css("width", "100%");
-		    	$("#js-sk").css("width", "70%");
-                $("#php-sk").css("width","65%");
-		    	$("#twig-sk").css("width","80%");
-		    	$("#grail-sk").css("width","50%");
-		    	$("#groov-sk").css("width","80%");
-		    	$("#c-sk").css("width","70%");
-				$("#pyth-sk").css("width","50%");
-			}
-		}
-	});
+        if (percentScroll <= 96) {
+            if (scrollPosition < slideTwo && scrollPosition > slideOne) {
+                $("#indicator").css("left", indicatorOne.leftOffset)
+                    .css("width", indicatorOne.width)
+                    .css("background-color", indicatorOne.color);
+            } else if (scrollPosition < slideThree && scrollPosition > slideTwo) {
+                $("#indicator").css("left", indicatorTwo.leftOffset)
+                    .css("width", indicatorTwo.width)
+                    .css("background-color", indicatorTwo.color);
+            } else if (scrollPosition < slideFour && scrollPosition > slideThree) {
+                $("#indicator").css("left", indicatorThree.leftOffset)
+                    .css("width", indicatorThree.width)
+                    .css("background-color", indicatorThree.color);
+                if (scrollPosition > (slideThree * 1.4)) {
+                    $("#html-sk").css("width", "100%");
+                    $("#css-sk").css("width", "100%");
+                    $("#js-sk").css("width", "70%");
+                    $("#php-sk").css("width", "65%");
+                    $("#twig-sk").css("width", "80%");
+                    $("#grail-sk").css("width", "50%");
+                    $("#groov-sk").css("width", "80%");
+                    $("#c-sk").css("width", "70%");
+                    $("#pyth-sk").css("width", "50%");
+                    progressBars = true;
+                }
+            } else if (scrollPosition < slideFive && scrollPosition > slideFour) {
+                $("#indicator").css("left", indicatorFour.leftOffset)
+                    .css("width", indicatorFour.width)
+                    .css("background-color", indicatorFour.color);
+            }
+        } else {
+            $("#indicator").css("left", indicatorFive.leftOffset)
+                .css("width", indicatorFive.width)
+                .css("background-color", indicatorFive.color);
+            if (!progressBars) {
+                $("#html-sk").css("width", "100%");
+                $("#css-sk").css("width", "100%");
+                $("#js-sk").css("width", "70%");
+                $("#php-sk").css("width", "65%");
+                $("#twig-sk").css("width", "80%");
+                $("#grail-sk").css("width", "50%");
+                $("#groov-sk").css("width", "80%");
+                $("#c-sk").css("width", "70%");
+                $("#pyth-sk").css("width", "50%");
+            }
+        }
+    });
 
     // smooth scrolling to anchors
     $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 500);
-            return false;
-          }
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+                return false;
+            }
         }
     });
 });
